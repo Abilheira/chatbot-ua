3d267780a2d1c31bc45a3cfd7e48b67554418c4a
jose <dizzeny13@gmail.com>
2026-05-21 13:28:50 +0100

    alteração

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,7 +34,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("https://chatbot-ua.onrender.com/chat", {
+      const res = await fetch("https://chatbot-ua-wqvd.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage }),
