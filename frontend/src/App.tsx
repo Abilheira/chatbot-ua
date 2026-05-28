527ce6c7634ecc9d14dc26d1255758d2e80bb74c
jose <dizzeny13@gmail.com>
2026-05-28 11:40:23 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -35,7 +35,7 @@ export default function App() {
   setMessage("");
 
   try {
-    const response = await fetch("http://chatbot-ua.vercel.app/chat", {
+    const response = await fetch("https://chatbot-ua-wqvd.onrender.com/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
