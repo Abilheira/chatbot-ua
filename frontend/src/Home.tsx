0dbcc550d1e7628d9fef95ca9a520115158bce88
jose <dizzeny13@gmail.com>
2026-06-16 11:22:05 +0100

    update

---
+++ frontend/src/Home.tsx
@@ -0,0 +1,74 @@
+import "./Home.css";
+
+type Props = {
+  onStart: () => void;
+  onSuggestionClick: (text: string) => void;
+};
+
+export default function Home({ onStart, onSuggestionClick }: Props) {
+  return (
+    <div className="home">
+
+      <div className="home-header">
+        <img src="/logobranco.png" className="home-logo" />
+      </div>
+
+      <div className="home-main">
+
+        <img src="/chatbot2.png" className="home-bot" />
+
+        <h1>
+          Olá! Tira as tuas<br />
+          dúvidas comigo :)
+        </h1>
+
+        {/* SUGESTÕES */}
+        <div className="home-section">
+
+          <h2>Sugestões Populares</h2>
+
+          <div className="suggestions">
+
+            <button onClick={() => onSuggestionClick("Como faço a minha matrícula?")}>
+              🎓 Como faço a minha matrícula?
+            </button>
+
+            <button onClick={() => onSuggestionClick("Quando começam as aulas?")}>
+              📅 Quando começam as aulas?
+            </button>
+
+            <button onClick={() => onSuggestionClick("Como consultar o meu horário?")}>
+              ⏰ Consultar horário
+            </button>
+
+            <button onClick={() => onSuggestionClick("Quais são os prazos de pagamento?")}>
+              📄 Prazos de pagamento
+            </button>
+
+          </div>
+        </div>
+
+        {/* ATALHOS */}
+        <div className="home-section">
+
+          <h2>Atalhos Rápidos</h2>
+
+          <div className="shortcuts">
+
+            <a href="https://paco.ua.pt/" target="_blank">📚 Portal Académico</a>
+            <a href="https://www.ua.pt/pt/sga/page/4618" target="_blank">📅 Calendário</a>
+            <a href="https://elearning.ua.pt/" target="_blank">📖 E-Learning</a>
+            <a href="https://www.ua.pt/pt/contactos-gerais" target="_blank">📞 Contactos UA</a>
+
+          </div>
+
+        </div>
+
+        <button className="start-btn" onClick={onStart}>
+          Começar ➜
+        </button>
+
+      </div>
+    </div>
+  );
+}
